e88bc0546bb23acd3d9d4b520caa650f63340f74
Behzad Shirani <shirani.developer@google.com>
2021-02-23 08:58:37 +0330

    delete folders

--- Todo-Mobx/todo-mobx/src/providers/store-provider.ts
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-import { TodoList } from "../stores/todo-list";
-
-export const StoreContext = createContext<TodoList>({} as TodoList);
-export const StoreProvider = StoreContext.Provider;
-
-export const useStore = (): TodoList => useContext(StoreContext);
